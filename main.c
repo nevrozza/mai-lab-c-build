da0fcd0e2202219f5764298c8233e9dff89ef20d
Artyom Matashkov <aimatashkov@mai.education>
2026-01-04 04:35:04 +0300

    add sample code

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
+int factorial(int n) {
+    if (n <= 1) return 1;
+    return n * factorial(n - 1);
+}
+
 int main(void) {
-    printf("Hi!");
+    const int num = 5;
+    const int result = factorial(num);
+    printf("%d! = %d\n", num, result);
     return 0;
 }
